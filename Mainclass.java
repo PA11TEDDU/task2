566b8e22b048c8c5a1c324f4d330b0f8bd2df7b7
PA11TEDDU <60224774+PA11TEDDU@users.noreply.github.com>
2020-07-15 22:41:58 +0530

    Update Mainclass.java

--- Mainclass.java
+++ Mainclass.java
@@ -1,71 +1,59 @@
 package pavan;
-
-// Abstract classes
-abstract class Aclass{
-	public abstract void amethod();
-}
-
-//Interfaces
-interface Maininterface{
-	void displayed();
-}
-
-//Inheritance
-class Imp extends Aclass implements Maininterface{
-	public void amethod() {
-		System.out.println("Implementation of Abstract Method of Abstract Class");
+import java.util.*;
+	abstract class Newyear
+	{
+	void menu()
+	{
+	System.out.println("MENU");
+	System.out.println("Chocolates Menu:");
+	System.out.println("1.Dairy Milk \n2.Eclairs Chocolates \n3.5-Star Chocolates \n4.no more Chocolates\n");}
+	void menu2(){
+	System.out.println("Sweets Menu:");
+	System.out.println("1.Jalebi \n2.Halwa \n3. Gulab Jamun \n4.no more sweets");
 	}
-	public void displayed() {
-		System.out.println("Implementation of Interface");
+	///void sort(int t,int price,int n);
+	 //void sort(int name,int p);
 	}
-	public void poly() {
-		System.out.println("method without polymorphism");
+	class Choco extends Newyear
+	{
+	float sort()
+	{   Scanner sc=new Scanner(System.in);
+	    System.out.println("enter the type of Chocolate you have");
+	    int c=sc.nextInt();
+	    System.out.println("enter number of chocolate pieces:");
+	    int n=sc.nextInt();
+	    int[] weight= new int[]{20,30,45,50};
+	    
+	    return weight[c]*n;
 	}
-	//Static Polymorphism
-	public void add(int a, int b)
-	    {
-	         int d = a+b;
-	 		System.out.println("addition is "+d);
-	    }
-	public void add(int a, int b, int c)  
-	    {
-	        int d=a+b+c;
-	         System.out.println("addition is "+d);
-	         
-	    }	
-	// Encapsulation
-	private String sname;
-	public String getSname() {
-		return sname;
+	    
 	}
-	public void setSname(String sname) {
-		this.sname = sname;
+	class sweets extends Newyear
+	{
+	float sort(int name,int n)
+	{
+	float weight_s=0;
+	int price[]={40,50,27,85};
+	return price[name]*n;
 	}
-	
-		
-}
-
-//Runtime Polymorphism
-class Inherit extends Imp{
-	public void poly() {
-		System.out.println("method after polymorphism(RunTime)");
 	}
-}
-
-
-
-
-public class Mainclass {
-    public static void main(String[] args) {
-	Imp i=new Imp();
-	i.amethod();
-	i.displayed();
-	i.add(10,20,30);
-	i.setSname("Pavan");  
-	System.out.println(i.getSname());  
-	Imp j=new Inherit();
-	j.poly();
-     
+	public class Main
+	{
+	public static void main(String[] args)
+	{
+	Scanner sc=new Scanner(System.in);
+	Choco c=new Choco();
+	sweets s=new sweets();
+	c.menu();
+	float c_wt=c.sort();
+	System.out.println("enter the name of sweet using its number given in menu:");
+	s.menu2();
+	int name=sc.nextInt();
+	System.out.println("enter number of sweets:");
+	int p=sc.nextInt();
+	float s_wt=s.sort(name,p);
+	System.out.println("toatl weight of gift:"+ (c_wt+s_wt));
+	System.out.println("total number of candies and sweets in gift:"+ (p));
+	}
 	}
 
-}
